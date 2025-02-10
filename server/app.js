45853e1764451844ef6ccded09cf1a700bcb660b
nkamalu620 <nkamalu96@gmail.com>
2025-02-09 21:16:24 -0500

    updated code in files and added code in assessmentController.js file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth.js');
-const employeeRoutes = require('./routes/api/employee-routes.js');
-const assessmentRoutes = require('./routes/api/assessment-routes.js');
+const employeeRoutes = require('./routes/api/employeeRoutes.js');
+const assessmentRoutes = require('./routes/api/assessmentRoutes.js');
 
 const app = express();
 
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/assessments', assessmentRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/src/controllers/assessmentController.js
+++ server/src/controllers/assessmentController.js
@@ -1,2 +1,56 @@
 const { assessment } = require('../Models/assessment.js');
 const { employee } = require('../Models/employee.js');
+
+const getAssessments = async (req, res) => {
+    try {
+        const assessments = await assessment.findAll();
+        res.status(200).json(assessments);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching assessments', error });
+    }
+};
+
+const getAssessmentsById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const assessment = await Assessment.findByPk(id, { include: employee });
+        if (!assessment) {
+          return res.status(404).json({ message: 'Assessment not found' });
+        }
+        res.status(200).json(assessment);
+      } catch (error) {
+        res.status(500).json({ message: 'Error fetching assessment', error });
+      }
+    };
+
+const createAssessment = async (req, res) => {
+    const { employeeId, stressLevel, date } = req.body;
+  try {
+    const employee = await Employee.findByPk(employeeId);
+    if (!employee) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const assessment = await Assessment.create({ employeeId, stressLevel, date });
+    res.status(201).json({ message: 'Assessment created successfully', assessment });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating assessment', error });
+  }
+};
+
+const deleteAssessment = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const assessment = await Assessment.findByPk(id);
+        if (!assessment) {
+          return res.status(404).json({ message: 'Assessment not found' });
+        }
+        await assessment.destroy();
+        res.status(200).json({ message: 'Assessment deleted successfully' });
+      } catch (error) {
+        res.status(500).json({ message: 'Error deleting assessment', error });
+      }
+    };
+
+    module.exports = { getAssessments, getAssessmentById, createAssessment, deleteAssessment };
+
+
