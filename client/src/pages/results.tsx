d4b5f5f683740f0519b90b2df43d789ff616d14b
nkamalu620 <nkamalu96@gmail.com>
2025-02-17 19:41:05 -0500

    updated styling

--- client/src/pages/results.tsx
+++ client/src/pages/results.tsx
@@ -11,8 +11,8 @@ const results: React.FC = () => {
         <>
         <div className="container results-page">
             <h2>Results</h2>
-            <p>Your Crashout meter is: {totalScore}</p>
-            <p>
+            <p className="message">Your Crashout meter is: {totalScore}</p>
+            <p className="message">
                 {totalScore <= 10 && 'Your crashout meter is low. Keep up the good work!'}
                 {totalScore > 10 && totalScore <= 20 && 'Your crashout meter is moderate. Consider some relaxation techniques.'}
                 {totalScore > 20 && 'Your crashout level is high. It might be a good idea to talk to a professional.'}
